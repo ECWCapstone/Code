4ed9c2661c505e8dd174b13cd4ded05e78adfff2
EthanEChristian <EthanEChristian@github.com>
2015-03-10 16:02:49 -0500

    Change Layout

--- epocInterface.py
+++ epocInterface.py
@@ -1,14 +1,37 @@
 #processes events as they come in
 from PyEpoc import EpocHandler, ERRCODE
-
+import threading
+import time
 
 class EpocInterface:
 
-	def __init__(self):
-		e_handler = EpocHandler()
+	def print_me(str_input):
+		print str_input
+
+	def __init__(self, funct=print_me):
+		# self.e_handler = EpocHandler()
+		self.handler_funct = funct
 
 	def connect(self):
-		print e_handler.EE_EngineConnect()
+		return self.e_handler.EE_EngineConnect()
+
+	def sample_gyro_data():
+		engine = EpocHandler()
+		print engine.EE_EngineConnect()
+
+		new_event = engine.EE_EmoEngineEventCreate()
+
+		while True:
+			state = engine.EE_EngineGetNextEvent(new_event)
+			gyro_data = engine.EE_HeadsetGetGyroDelta(0)
+			self.handler_funct(gyro_data[1])
+
+	def run(self):
+		threading.Thread(target=sample_gyro_data).start()
+
+	
+			
+
 
 	#consumer function
 	#directions processed by headset represented in tupples
@@ -17,7 +40,7 @@ class EpocInterface:
 	#coords is a hash table where X: delta(X), and Y:delta(Y)
 	#overall: returns percentage of each change in direction while ignoring values under 25%
 
-	def processGyroData(tup):
+	def processGyroData(self,tup):
 		coords = {"x": tup[0], "y": tup[1]}
 		Directions = [] #array that holds directions
 		sumDirect = float(abs(coords['x']) + abs(coords['y']))
