86f00f61294a96671a19a3ace4d7cf6e545995b5
EthanEChristian <EthanEChristian@github.com>
2015-04-09 14:55:48 -0500

    Add delay to the summation

--- main.py
+++ main.py
@@ -46,26 +46,32 @@ class state:
 	x = 0
 	y = 0
 	count = 0
-	count_final = 33    # using "time python main.py" it was 11.2359 samples per ms And the drone samples every 3 ms  therefore 33 samples is good
+	count_final = 75    # using "time python main.py" it was 11.2359 samples per ms And the drone samples every 3 ms  therefore 33 samples is good
+						# or not?
+	def add_x(self,newX):
+		self.x = newX + self.x
 
-	def add_x(newX):
-		x = newX + x
+	def add_y(self,newY):
+		self.y = newY + self.y
 
-	def add_y(newY):
-		y = newY + y
+	def inc_count(self):
+		self.count = self.count + 1
 
-	def inc_count():
-		count = count + 1
-
-	def zero_count():
-		count = 0
+	def zero_count(self):
+		self.count = 0
 
 def write_copter(copter, stat):
 	def fly_copter(tup):
 		coords = {"x": tup[0], "y": tup[1]}
+
+		if abs(coords['x'])< 5 and abs(coords['y'])<5:
+			stat.inc_count()
+			return
 		stat.add_x(coords['x'])
 		stat.add_y(coords['y'])
 		stat.inc_count()
+
+
 
 		if stat.count >= stat.count_final:
 			sumDirect = float(abs(stat.x) + abs(stat.y))
@@ -99,56 +105,56 @@ def write_copter(copter, stat):
 # Drone
 ###
 
-# drone = ardrone.ARDrone()
-# drone.setup()
-# drone.flat_trims()
-# time.sleep(3)
-# print 'drone set'
-# drone.set_speed(.1)
+drone = ardrone.ARDrone()
+drone.setup()
+drone.flat_trims()
+time.sleep(3)
+print 'drone set'
+drone.set_speed(.1)
 
-# interface = EpocInterface(write_copter(drone))
-# interface.run()
-# print 'main is in control'
+interface = EpocInterface(write_copter(drone,state()))
+interface.run()
+print 'main is in control'
 
-# while True:
+while True:
 
-# 	try: 
-# 		cmd = getch()
+	try: 
+		cmd = getch()
 
-# 		if ord(cmd) == 3:# looking for ctrl + c
-# 			drone.disconect()
-# 			sys.exit(0)
+		if ord(cmd) == 3:# looking for ctrl + c
+			drone.disconect()
+			sys.exit(0)
 
 		
-# 		if cmd == 'g':
-# 			drone.land()
-# 		elif cmd == 't':
-# 			print "Taking off"
-# 			drone.take_off()
-# 		elif cmd == 'a':
-# 			drone.left()
-# 		elif cmd == 'd':
-# 			drone.right()
-# 		elif cmd =='w':
-# 			drone.forward()
-# 		elif cmd == 's':
-# 			drone.backward()
-# 		elif cmd == 'i':
-# 			drone.up()
-# 		elif cmd == 'k':
-# 			drone.down()
-# 		elif cmd == 'j':
-# 			drone.rotate_left()
-# 		elif cmd == 'l':
-# 			drone.rotate_right()
-# 		elif cmd == 'e':
-# 			drone.emergency_stop() 
-# 		else:
-# 			pass
-# 	except Exception as e:
-# 		print type(e)
-# 		print e.message           
-# 		print "It was Will's fault"
+		if cmd == 'g':
+			drone.land()
+		elif cmd == 't':
+			print "Taking off"
+			drone.take_off()
+		elif cmd == 'a':
+			drone.left()
+		elif cmd == 'd':
+			drone.right()
+		elif cmd =='w':
+			drone.forward()
+		elif cmd == 's':
+			drone.backward()
+		elif cmd == 'i':
+			drone.up()
+		elif cmd == 'k':
+			drone.down()
+		elif cmd == 'j':
+			drone.rotate_left()
+		elif cmd == 'l':
+			drone.rotate_right()
+		elif cmd == 'e':
+			drone.emergency_stop() 
+		else:
+			pass
+	except Exception as e:
+		print type(e)
+		print e.message           
+		print "It was Will's fault"
 
 
 
@@ -168,5 +174,5 @@ def write_copter(copter, stat):
 # counter
 ###
 
-interface = EpocInterface(write_count_func(counter()))
-interface.run()
+# interface = EpocInterface(write_count_func(counter()))
+# interface.run()
